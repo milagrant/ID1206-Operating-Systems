a6973771abd16ebb725af1ec4cf51933c1b081e8
mila <milag@kth.se>
2017-11-22 15:53:55 +0100

    stage 2

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#![feature(unique)]
+#![feature(lang_items)]
+#![no_std]
+#![feature(const_fn)]
+#![feature(const_unique_new)]
+
+extern crate volatile;
+extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub extern fn rust_main()
+{
+     // ATTENTION: we have a very small stack and no guard page
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
+
+    loop{}
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
---
+++ target/x86_64-blog_os/debug/libblog_os.d
@@ -0,0 +1,1 @@
+/home/mila/Documents/project/target/x86_64-blog_os/debug/libblog_os.a: /home/mila/Documents/project/src/lib.rs /home/mila/Documents/project/src/vga_buffer.rs
