feebb06954c7963108e3de372089beedcfede34e
mila <milag@kth.se>
2017-11-27 14:44:21 +0100

    second of third

--- src/lib.rs
+++ src/lib.rs
@@ -13,68 +13,76 @@ extern crate multiboot2;
 mod vga_buffer;
 mod memory;
 
+#[macro_use]
+extern crate bitflags;
+extern crate x86_64;
+
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize)
 {
 
-	 use memory::FrameAllocator;
-     // ATTENTION: we have a very small stack and no guard page
+    use memory::FrameAllocator;
+ 
     vga_buffer::clear_screen();
     println!("Hello World{}", "!");
     println!("{}", { println!("inner"); "outer" });
 
-    
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag()
+       .expect("Memory map tag required");
 
-let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
-let memory_map_tag = boot_info.memory_map_tag()
-    .expect("Memory map tag required");
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas()
+    {
+       println!("    start: 0x{:x}, length: 0x{:x}",
+       area.base_addr, area.length);
+    }
 
-println!("memory areas:");
-for area in memory_map_tag.memory_areas() {
-    println!("    start: 0x{:x}, length: 0x{:x}",
-        area.base_addr, area.length);
-}
+   let elf_sections_tag = boot_info.elf_sections_tag()
+       .expect("Elf-sections tag required");
 
-let elf_sections_tag = boot_info.elf_sections_tag()
-    .expect("Elf-sections tag required");
-
-println!("kernel sections:");
-for section in elf_sections_tag.sections() {
+   println!("kernel sections:");
+   for section in elf_sections_tag.sections()
+   {
     println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
         section.addr, section.size, section.flags);
-}
-
-let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+   } 
+  
+   let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
     .min().unwrap();
-let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+   let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
     .max().unwrap();
 
-println!("kernel start: 0x{}", kernel_start);
-println!("kernel end: 0x{}", kernel_end);
+    println!("kernel start: 0x{}", kernel_start);
+    println!("kernel end: 0x{}", kernel_end);
 
-let multiboot_start = multiboot_information_address;
-let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
 
-println!("multiboot start: 0x{}", multiboot_start);
-println!("multiboot end: 0x{}", multiboot_end);
-
+    println!("multiboot start: 0x{}", multiboot_start);
+    println!("multiboot end: 0x{}", multiboot_end); 
 
     let mut frame_allocator = memory::AreaFrameAllocator::new(
         kernel_start as usize, kernel_end as usize, multiboot_start,
         multiboot_end, memory_map_tag.memory_areas());
+	memory::test_paging(&mut frame_allocator);
 
-    for i in 0.. {
-        if let None = frame_allocator.allocate_frame() {
+    for i in 0..
+    {
+        if let None = frame_allocator.allocate_frame()
+	{
             println!("allocated {} frames", i);
             break;
         }
-}
+   }
 
-loop{}
-/*
+   loop{}
+   
+    /*
     let y = 4;
     let x = fix(y);
-    println!("Fibboniacci of {} is {}", y, x);*/
+    println!("Fibboniacci of {} is {}", y, x);
+    */
     
 }
 
@@ -104,8 +112,6 @@ pub fn fib(n:u32) -> (u32, u32)
 		(f1 + f2, f1)
 	}
 }
-
-
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 
